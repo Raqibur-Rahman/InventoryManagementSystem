e03ca09da7ff9f38f59885c1b4bd05693f4ab834
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-02-28 14:06:17 +0600

    Add project files.

---
+++ CRUD_WEB_API_DEMO/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_WEB_API_DEMO.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRUD_WEB_API_DEMO/Models/RegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD_WEB_API_DEMO.Models
+{
+    public class RegistrationModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
